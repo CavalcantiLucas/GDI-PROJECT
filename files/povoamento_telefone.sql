16ce13c7f2093874762b22029ba509f6cb70db81
Raíssa Heimann <raissaheimann@gmail.com>
2022-08-30 16:48:47 -0300

    added povoamento Numero_Telefone (main)

---
+++ files/povoamento_telefone.sql
@@ -0,0 +1,121 @@
+-- povoamento Numero_Telefone
+
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('51871590035', '(81)9874-5632');
+
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('51871590035', '(81)9499-1316');
+
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('95438868069', '(47)3972-9080');
+
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('95438868069', '(47)3142-1485');
+
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('95068505005', '(63)2826-7292');
+
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('95068505005', '(27)2703-8803');
+
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('42777797005', '(89)2360-5288');
+
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('42777797005', '(98)3280-8086');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('32798587042', '(85)2061-8663');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('32798587042', '(95)2708-2183');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('99942745033', '(66)2257-3548');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('61888835044', '(83)2674-1757');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('99518963088', '(97)3681-1333');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('29014744030', '(61)2723-7967');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('16790533028', '(68)2871-1567');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('35170942001', '(63)3077-8153');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('66559107060', '(84)3323-9848');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('78366263002', '(49)2665-5117');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('96627054060', '(85)3644-4381');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('49869393004', '(82)3123-1501');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('55566621111', '(83)2023-1391');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('19321816283', '(85)2756-2416');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('94044687544', '(67)3638-7834');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('34864572097', '(98)3181-5805');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('42824439602', '(68)3415-5355');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('96382647217', '(83)2025-1552');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('17932577527', '(83)2362-4256');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('38612505780', '(65)3117-9564');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('93405446929', '(24)2678-6881');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('26744842684', '(18)2998-3706');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('77648271427', '(61)3150-7096');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('62538131103', '(62)3516-0550');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('88634284255', '(63)2433-8382');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('62982197499', '(79)3823-5373');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('18956810818', '(65)2761-2123');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('06027847514', '(12)3413-5118');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('85997072215', '(88)2965-0260');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('53885643715', '(32)3458-4154');
+    
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('45836067376', '(22)2076-2428');
+
+INSERT INTO Numero_Telefone(cpf_pessoa, telefone)
+    VALUES ('61245137220', '(35)3146-8727');
