f145f4612a47f7bc04f815faffceec1660b03cb0
artursanntos <arturcs2001@gmail.com>
2022-08-30 13:14:02 -0300

    added povoamento Funcionario (main)

---
+++ files/povoamento_funcionario.sql
@@ -0,0 +1,57 @@
+-- povoamento Funcionario
+
+-- Casa 1
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('99942745033', 'Caixa', '40658419000150',  '42777797005');
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('51871590035', 'Dealer', '40658419000150',  '95068505005');
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('95438868069', 'Dealer', '40658419000150',  '95068505005');
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('95068505005', 'Supervisor', '40658419000150',  NULL);
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('42777797005', 'Supervisor', '40658419000150',  NULL);
+
+
+-- Casa 2
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('32798587042', 'Caixa', '07829772000180',  '16790533028');
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('61888835044', 'Caixa', '07829772000180',  '16790533028');
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('99518963088', 'Dealer', '07829772000180',  '16790533028');
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('29014744030', 'Dealer', '07829772000180',  '16790533028');
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('16790533028', 'Supervisor', '07829772000180',  NULL);
+
+
+-- Casa 3
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('35170942001', 'Caixa', '89619720000143',  '49869393004');
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('66559107060', 'Dealer', '89619720000143',  '49869393004');
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('78366263002', 'Dealer', '89619720000143',  '49869393004');
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('96627054060', 'Dealer', '89619720000143',  '49869393004');
+
+INSERT INTO Funcionario(cpf_funcionario, cargo, cnpj_casa, cpf_supervisor)
+    VALUES ('49869393004', 'Supervisor', '89619720000143',  NULL);
+
+
+
