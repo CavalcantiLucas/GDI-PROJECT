cb82032c2311b35ea4e4db9c71c64b1dbb3c575f
Matheus Frej <matheusfrej@hotmail.com>
2022-08-31 14:43:08 -0300

    Povoamento da tabela Joga

---
+++ files/povoamento_joga.sql
@@ -0,0 +1,60 @@
+
+-- Grupinho jogando poker
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('55566621111', 3, '"2022-08-31",  "19:10:01"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('94044687544', 3, '"2022-08-31",  "19:10:01"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('34864572097', 3, '"2022-08-31",  "19:10:01"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('96382647217', 3, '"2022-08-31",  "19:10:01"')
+
+-- Jogadores de Caça-Níquel
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('96382647217', 5, '"2022-08-20",  "23:50:34"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('62538131103', 5, '"2022-02-31",  "19:45:56"')
+
+-- Jogadores de BlackJack
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('17932577527', 1, '"2022-05-26",  "13:40:34"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('17932577527', 1, '"2022-05-27",  "13:42:34"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('17932577527', 1, '"2022-05-28",  "14:50:12"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('61245137220', 1, '"2022-10-10",  "20:10:30"')
+
+-- Jogadores de Roleta
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('06027847514', 2, '"2022-08-30",  "21:10:30"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('85997072215', 2, '"2022-07-23",  "10:42:45"')
+
+-- Jogadores de Roda da Fortuna
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('26744842684', 4, '"2022-04-28",  "21:10:18"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('26744842684', 4, '"2022-05-12",  "20:17:20"')
+
+-- Jogadores de Pachinko
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('77648271427', 6, '"2022-08-23",  "19:12:00"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('93405446929', 6, '"2022-05-12",  "20:17:20"')
+
+-- Grupinho jogando Craps 
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('88634284255', 7, '"2022-06-12",  "23:15:14"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('62982197499', 7, '"2022-06-12",  "23:15:14"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('18956810818', 7, '"2022-06-12",  "23:15:14"')
+
+-- Jogadores de Baccarat
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('53885643715', 8, '"2022-01-24",  "21:30:12"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('45836067376', 8, '"2022-06-05",  "22:42:57"')
+INSERT INTO Joga(cpf_jogador, jogo_id, datahora)
+    VALUES ('55566621111', 8, '"2022-04-07",  "19:10:11"')
